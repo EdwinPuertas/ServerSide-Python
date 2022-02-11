9c0d182172fe704d4dc6c4d7781b3317b63e4470
Edwin Puertas <eapuerta@gmail.com>
2022-02-11 07:26:15 -0500

    Update all

--- web/app.py
+++ web/app.py
@@ -1,16 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
+from logic.person import Person
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+model = []
 
 
 @app.route("/")
 def index():
-    return 'Hola Mundo'
+    return render_template('index.html')
 
 
-@app.route('/home')
-def home():
-    return render_template('home.html', title="Home", content="Hello, World!")
+@app.route('/person', methods=['GET'])
+def person():
+    return render_template('person.html')
+
+
+@app.route('/person_detail', methods=['POST'])
+def person_detail():
+    id_person = request.form['id_person']
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    p = Person(id_person=id_person, name=first_name, last_name=last_name)
+    model.append(p)
+    return render_template('person_detail.html', value=p)
+
+
+@app.route('/people')
+def people():
+    data = [(i.id_person, i.name, i.last_name) for i in model]
+    print(data)
+    return render_template('people.html', value=data)
 
 
 if __name__ == '__main__':
